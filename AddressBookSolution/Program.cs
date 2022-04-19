aa1a3c7eb50f47326c4f6685ddb36e9f01e23ab9
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-19 14:12:51 +0530

    [VINAY] Add: Displaying Welcome Message

---
+++ AddressBookSolution/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using AddressBookSolution;
+class program
+{
+    public static void Main(String[] args)
+    { 
+    AddressBookMain address=new AddressBookMain();
+        string message = address.WelcomeMessage();
+        Console.WriteLine("******** "+message+" **********");
+    }
+}
