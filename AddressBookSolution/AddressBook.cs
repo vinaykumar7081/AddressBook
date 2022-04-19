5f2bcc884f148a7efa4e1338dd8cf88b91ee7290
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-19 17:21:35 +0530

    [VINAY] Add: Displaying Multiple Address Using ArrayList

--- AddressBookSolution/AddressBook.cs
+++ AddressBookSolution/AddressBook.cs
@@ -3,20 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
 
 namespace AddressBookSolution
 {
     public class AddressBook
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string State { get; set; }
-        public string EmailAddress { get; set; }    
-        public int PostalCode { get; set; }
-        public long MobileNumber { get; set; }
-
-
-
+    List<Contact> addressBook=new List<Contact>();
+        public AddressBook()
+        {
+            Contact address1 = new Contact()
+            {
+              FirstName = Console.ReadLine(),
+              LastName = Console.ReadLine(),
+              Address = Console.ReadLine(),
+              State = Console.ReadLine(),
+              EmailAddress = Console.ReadLine(),
+              PostalCode = Convert.ToInt32(Console.ReadLine()),
+              MobileNumber = Convert.ToInt64(Console.ReadLine())
+            };
+            Contact address2 = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                State = Console.ReadLine(),
+                EmailAddress = Console.ReadLine(),
+                PostalCode = Convert.ToInt32(Console.ReadLine()),
+                MobileNumber = Convert.ToInt64(Console.ReadLine())
+            };
+            addressBook.Add(address1);
+            addressBook.Add(address2);
+        }
+        public void AddContactToAddressBook(Contact contact)
+        { 
+        addressBook.Add(contact);
+        }
+        public void Display()
+        {
+            foreach (var contact in addressBook)
+            { 
+            Console.WriteLine(contact.FirstName+" "+contact.LastName+" "+contact.Address+" "+contact.State+" "+contact.EmailAddress+" "+" "+contact.PostalCode+" "+contact.MobileNumber  );
+            }
+        }
+        
     }
 }
--- AddressBookSolution/Program.cs
+++ AddressBookSolution/Program.cs
@@ -5,15 +5,7 @@ class program
     public static void Main(String[] args)
     {
         Console.WriteLine("Enter the Address Details");
-    AddressBookContact address=new AddressBookContact ();
-        address.FirstName =Console.ReadLine();
-        address.LastName =Console.ReadLine();
-        address.Address=Console.ReadLine();
-        address.State = Console.ReadLine();
-        address.EmailAddress = Console.ReadLine();
-        address.PostalCode =Convert.ToInt32(Console.ReadLine());
-        address.MobileNumber =Convert.ToInt64 (Console.ReadLine());
-        Console.WriteLine(address.FirstName+" "+ address.LastName+" "+ address.Address+" "+ address.State+" "+ address.EmailAddress+" "+ address.PostalCode+" "+ address.MobileNumber);
-        
+          AddressBook addressBook = new AddressBook();
+        addressBook.Display();
     }
 }
