f9f7662352ed4f29ff2a5300a38a322fedfd1fcf
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-19 15:13:04 +0530

    [VINAY] Add:Displaying Full Address Of one Visitor

--- AddressBookSolution/Program.cs
+++ AddressBookSolution/Program.cs
@@ -3,9 +3,17 @@ using AddressBookSolution;
 class program
 {
     public static void Main(String[] args)
-    { 
-    AddressBookMain address=new AddressBookMain();
-        string message = address.WelcomeMessage();
-        Console.WriteLine("******** "+message+" **********");
+    {
+        Console.WriteLine("Enter the Address Details");
+    AddressBook address=new AddressBook();
+        address.FirstName =Console.ReadLine();
+        address.LastName =Console.ReadLine();
+        address.Address=Console.ReadLine();
+        address.State = Console.ReadLine();
+        address.EmailAddress = Console.ReadLine();
+        address.PostalCode =Convert.ToInt32(Console.ReadLine());
+        address.MobileNumber =Convert.ToInt64 (Console.ReadLine());
+        Console.WriteLine(address.FirstName+" "+ address.LastName+" "+ address.Address+" "+ address.State+" "+ address.EmailAddress+" "+ address.PostalCode+" "+ address.MobileNumber);
+        
     }
 }
